bb94cf28787280c37641e598d29db577bf82cd9c
Abhishek De <deabhishek06@gmail.com>
2024-02-28 12:45:27 +0530

    Created login signup pages and added react-router-dom in our project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignUp from '../features/auth/components/SignUp'
+
+const SignUpPage = () => {
+  return (
+    <SignUp/>
+  )
+}
+
+export default SignUpPage
