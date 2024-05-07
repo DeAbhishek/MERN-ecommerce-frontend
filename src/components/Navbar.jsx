a80d4df726fea8fcf264afd03bfdef7c3b4c3bd0
Abhishek <deabhishek06@gmail.com>
2024-05-06 22:43:39 -0700

    Mobile navbar change as per Admin and user.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -182,23 +182,26 @@ const Navbar = ({ children, pageName = "" }) => {
 
             <Disclosure.Panel className="md:hidden">
               <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                {navigation.map((item) => (
-                  <Link to={item.link} key={item.name}>
-                    <Disclosure.Button
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "block rounded-md px-3 py-2 text-base font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  </Link>
-                ))}
+                {navigation.map(
+                  (item) =>
+                    item[loginRes?.role] && (
+                      <Link to={item.link} key={item.name}>
+                        <Disclosure.Button
+                          as="a"
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-900 text-white"
+                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "block rounded-md px-3 py-2 text-base font-medium"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </Disclosure.Button>
+                      </Link>
+                    )
+                )}
               </div>
               <div className="border-t border-gray-700 pb-3 pt-4">
                 <div className="flex items-center px-5">
